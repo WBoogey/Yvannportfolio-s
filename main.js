ceed3ec6785e2cec6c9dc3c8719c3e6e0a278707
Wboogey <ehourathegreat@gmail.com>
2024-07-02 10:16:16 +0200

    taille logo mobile

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-const Hamburger = document.querySelector('.Hamburger')
-
-const navLinks = document.querySelector('.navlinks')
+const hamburger = document.querySelector('.Hamburger');
+const navLinks = document.querySelector('.navlinks');
+const navLinksItems = document.querySelectorAll('.navlinks-items az');
 
 const navToggle = () => {
-    navLinks.classList.toggle("open")
+    navLinks.classList.toggle("open");
     if (navLinks.classList.contains("open")) {
-        console.log("actif")
-        document.querySelector(".Hamburger  .material-symbols-outlined ").innerHTML= "close"
+        console.log("actif");
+        document.querySelector(".Hamburger .material-symbols-outlined").innerHTML = "close";
     } else {
-        document.querySelector(" .Hamburger .material-symbols-outlined ").innerHTML= "menu"
+        document.querySelector(".Hamburger .material-symbols-outlined").innerHTML = "menu";
     }
-}
+};
 
-Hamburger.addEventListener('click', navToggle)
+hamburger.addEventListener('click', navToggle);
+
+navLinksItems.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("open");
+        console.log("remove");
+    });
+});
+
+const toggle = () => {
+    console.log('remove');
+};
+
+navLinks.addEventListener("click", toggle);
+
+console.log(navLinksItems)
+console.log(hamburger)

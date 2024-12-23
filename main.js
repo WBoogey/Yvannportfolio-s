891b49734bd5de29ef63480d0c5fee9d953176a7
Ehoura Christ-Yvann <w_ehoura@hetic.eu>
2024-12-23 21:40:11 +0100

    fix bug

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
 const hamburger = document.querySelector('.Hamburger');
 const navLinks = document.querySelector('.navlinks');
-const navLinksItems = document.querySelectorAll('.navlinks-items az');
+const navLinksItems = document.querySelectorAll('.navlinks-items a');
 
+// Fonction pour ouvrir/fermer le menu
 const navToggle = () => {
-    navLinks.classList.toggle("open");
-    if (navLinks.classList.contains("open")) {
-        console.log("actif");
-        document.querySelector(".Hamburger .material-symbols-outlined").innerHTML = "close";
-    } else {
-        document.querySelector(".Hamburger .material-symbols-outlined").innerHTML = "menu";
-    }
+  navLinks.classList.toggle("open");
+  
+  const icon = document.querySelector(".Hamburger .material-symbols-outlined");
+  if (navLinks.classList.contains("open")) {
+    icon.innerHTML = "close";
+  } else {
+    icon.innerHTML = "menu";
+  }
 };
 
 hamburger.addEventListener('click', navToggle);
 
+// Fermer le menu lorsqu'un lien est cliqué
 navLinksItems.forEach(link => {
-    link.addEventListener("click", () => {
-        navLinks.classList.remove("open");
-        console.log("remove");
-    });
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("open");
+
+    const icon = document.querySelector(".Hamburger .material-symbols-outlined");
+    icon.innerHTML = "menu";
+  });
 });
-
-const toggle = () => {
-    console.log('remove');
-};
-
-navLinks.addEventListener("click", toggle);
-
-console.log(navLinksItems)
-console.log(hamburger)
